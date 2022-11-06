944d322de1484f6f677e82e1166195a68cf85f30
Vivek Rajput <indvivekrajput@gmail.com>
2022-11-06 14:24:21 +0530

    adding login functionality

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,70 @@
 
 const express = require("express")
+const bcrypt = require('bcrypt');
 const {connection}=require("./config/db")
+const {UserModel} =require("./models/UserModel")
+const jwt = require("jsonwebtoken")
 const app = express()
 
+require("dotenv").config()
 app.use(express.json())
 
 app.get("/", (req, res) => {
     res.send("home")
 })
 
-app.post("/signup",(req,res)=>{
+app.post("/signup",async(req,res)=>{
     const {name,email,password}=req.body;
     console.log(name,email,password)
+ 
+    const isUser= await UserModel.findOne({email})
+    if(isUser){
+        res.send("user already exists , please try to logging up")
+    }
+
+    bcrypt.hash(password, 4,async function(err,hash) {
+      if(err){
+        res.send("please try again")
+      }
+
+
+      const new_user = new UserModel({
+          name,
+          email,
+          password:hash
+      })
+      try{
+         await new_user.save()
+         res.send("sign up successfully")
+      }
+      catch(err){
+          res.send("error occur")
+      }
+       
+    });
+})
+
+
+//login
+
+app.post("/login", async(req,res)=>{
+    const {email,password}=req.body
+    const user = await UserModel.findOne({email})
+    const hash_password= user.password;
+    const user_id= user._id;
+    console.log(user)
+    bcrypt.compare(password,hash_password,function(err,result){
+        if(err){
+            res.send("try agai later")
+        }
+        if(result){
+            var token = jwt.sign({user_id}, process.env.SECRET_KEY);
+            res.send({msg:"login successfull",token})
+        }
+        else{
+            res.send("login failed")
+        }
+    })
 })
 
 app.listen(8000, async() => {
---
+++ backend/models/UserModel.js
@@ -0,0 +1,12 @@
+
+const mongoose = require("mongoose")
+
+const useSchema= new mongoose.Schema({
+    name:{type:String, require:true},
+    email:{type:String, require:true},
+    password:{type:String, require:true},
+})
+
+const UserModel = mongoose.model("bmiAuth", useSchema)
+
+module.exports={UserModel}
